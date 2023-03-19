06742b19eb005bfbd9362d6b57615b186a0cc8d5
Thomas NISI <nisi.thomas@outlook.fr>
2023-03-19 22:33:26 +0100

    list all articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     #[Route('/', name: 'app_blog')]
-    public function index(): Response
+
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAll();
+
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'articles' => $articles,
         ]);
     }
 }
