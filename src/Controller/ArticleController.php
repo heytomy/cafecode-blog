26d10fca7bce56a82c6148f9b8cf6c83e9e9dd4c
Thomas NISI <nisi.thomas@outlook.fr>
2023-03-26 23:25:06 +0200

    (❁´◡`❁)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,10 +6,11 @@ use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
@@ -37,15 +38,16 @@ class ArticleController extends AbstractController
 
     #[IsGranted('ROLE_ADMIN')]
     #[Route('/new', name: 'article_new')]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager,  Security $security): Response
     {
 
     $article = new Article();
-    $form = $this->createForm(ArticleType::class, $article);
 
+    $form = $this->createForm(ArticleType::class, $article);
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
+        $article->setAuthor($security->getUser());
         $entityManager->persist($article);
         $entityManager->flush();
 
